d7a8d6ac9765784514a9e48cf3ea46b71288bbcc
olaszek <kopyto.aleksandra@gmail.com>
2020-03-14 18:29:36 +0100

    2 oldest ages - kata

---
+++ 7kyu/Two Oldest Ages.js
@@ -0,0 +1,16 @@
+//***     Two Oldest Ages    ***  
+
+//The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+//
+//The order of the numbers passed in could be any order. The array will always include at least 2 items.
+//
+//For example:
+//    twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+
+// my solution :
+function twoOldestAges(ages){
+  const twoNumb = ages.sort(function(a, b){return a - b});
+  return [twoNumb[ages.length-2], twoNumb[ages.length-1]];
+  
+}
